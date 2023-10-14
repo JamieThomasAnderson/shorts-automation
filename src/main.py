36ad827054103d6e83798698420959a63d73c481
Jamie Anderson <jamiethomasanderson@protonmail.com>
2023-10-14 13:32:15 +0000

    initial commit

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ import pickle
 import re
 import random
 import openai
-import textwrap
-from nltk.tokenize import sent_tokenize
 from post import post_to_tiktok
 
 from selenium import webdriver
@@ -20,57 +18,58 @@ import edge_tts
 from edge_tts import VoicesManager
 import pysubs2
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
+from PIL import Image, ImageDraw, ImageFont
 import cv2
 
 from PIL import Image
 from moviepy import *
 from moviepy.editor import *
-from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
-from moviepy.editor import concatenate_audioclips, AudioFileClip
+from moviepy.editor import AudioFileClip
 
-def reddit_init():
 
-    with open('reddit_keys.json', 'r') as keys:
-        data = json.load(keys)
-    
-    client_id = data['client_id']
-    client_secret = data['client_secret']
-    user_agent = data['user_agent']
+def load_config(config_path="config.json"):
+    with open(config_path, "r") as f:
+        return json.load(f)
 
+
+def reddit_init(client_id, client_secret, user_agent):
     reddit = praw.Reddit(
-            client_id = client_id,
-            client_secret = client_secret,
-            user_agent = user_agent
-            )
-    
+        client_id=client_id, client_secret=client_secret, user_agent=user_agent
+    )
+
     return reddit
 
-def load_seen():
 
+def load_seen(seen_path="./data/seen.pkl"):
     seen = []
 
-    if os.path.isfile('seen.pkl'):
-        with open('seen.pkl', 'rb') as f:
+    if os.path.isfile(seen_path):
+        with open("seen.pkl", "rb") as f:
             seen = pickle.load(f)
 
     return seen
 
-def dump_seen(seen):
+def clean_tmp(tmp_path="./tmp"):
+    for filename in os.listdir(tmp_path):
+        file_path = os.path.join(tmp_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
-    with open('seen.pkl', 'wb') as f:
+
+def dump_seen(seen, seen_path="./data/seen.pkl"):
+    with open(seen_path, "wb") as f:
         pickle.dump(seen, f)
 
+
 def clean_post(post_text):
-    post_text = re.sub(r'http\S+', '', post_text)
-    post_text = re.sub(r'[^a-zA-Z0-9.,\s]', '', post_text)
+    post_text = re.sub(r"http\S+", "", post_text)
+    post_text = re.sub(r"[^a-zA-Z0-9.,\s]", "", post_text)
     post_text = post_text.strip().replace("\n", "")
     return post_text
-    
 
-def get_post(reddit, sub="confession", time_filter="all"):
-    
-    seen = load_seen()
+
+def get_post(reddit, sub="confession", time_filter="all", seen_path="./data/seen.pkl"):
+    seen = load_seen(seen_path=seen_path)
 
     BATCH_SIZE = 10
 
@@ -78,16 +77,17 @@ def get_post(reddit, sub="confession", time_filter="all"):
 
     while True:
         for post in subreddit.top(time_filter=time_filter, limit=BATCH_SIZE):
-            if post.id not in seen and len(post.selftext.split()) < 1600:
+            if post.id not in seen and len(post.selftext.split()) < 1000:
                 seen.append(post.id)
-                dump_seen(seen)
+                dump_seen(seen, seen_path=seen_path)
                 return post.title, post.url, clean_post(post.selftext)
         BATCH_SIZE *= 2
+
 
 def get_image(name, username, tweet_content, output_path="post.png"):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    
+
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_window_size(1920, 1080)
     driver.get("https://www.tweetgen.com/create/tweet-classic.html")
@@ -95,7 +95,7 @@ def get_image(name, username, tweet_content, output_path="post.png"):
 
     driver.find_element(By.ID, "nameInput").send_keys(name)
     driver.find_element(By.ID, "usernameInput").send_keys(username)
-    driver.find_element(By.ID, "tweetTextInput").send_keys(tweet_content)    
+    driver.find_element(By.ID, "tweetTextInput").send_keys(tweet_content)
     time.sleep(10)
 
     tweet_container = driver.find_element(By.ID, "tweetInnerContainer")
@@ -103,7 +103,14 @@ def get_image(name, username, tweet_content, output_path="post.png"):
 
     driver.quit()
 
-def get_voiceover(post_text, speed=1.2, language="en", gender="Male", output_audio="voiceover_slow.mp3", output_subs="voiceover.vtt"):
+
+def get_voiceover(
+    post_text,
+    language="en",
+    gender="Male",
+    output_audio="voiceover.mp3",
+    output_subs="voiceover.vtt",
+):
     async def async_get_voiceover():
         voices = await VoicesManager.create()
 
@@ -123,7 +130,9 @@ def get_voiceover(post_text, speed=1.2, language="en", gender="Male", output_aud
                 if chunk["type"] == "audio":
                     audio_file.write(chunk["data"])
                 elif chunk["type"] == "WordBoundary":
-                    submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
+                    submaker.create_sub(
+                        (chunk["offset"], chunk["duration"]), chunk["text"]
+                    )
 
         with open(output_subs, "w", encoding="utf-8") as subs_file:
             subs_file.write(submaker.generate_subs())
@@ -133,40 +142,34 @@ def get_voiceover(post_text, speed=1.2, language="en", gender="Male", output_aud
         loop.run_until_complete(async_get_voiceover())
     finally:
         loop.close()
-    subprocess.run([
-        'ffmpeg', 
-        '-y', 
-        '-i', 
-        output_audio, 
-        '-filter:a', 
-        f"atempo={speed}", 
-        'voiceover.mp3'
-        ])
 
 
 def process_image(img_path):
-    img = Image.open(img_path)  
+    img = Image.open(img_path)
     width, height = img.size
 
     padding = 5
-    padded_img = Image.new('RGBA', (width + padding, height + padding), (0, 0, 0, 0))
+    padded_img = Image.new("RGBA", (width + padding, height + padding), (0, 0, 0, 0))
     padded_img.paste(img, (padding // 2, padding // 2))
     padded_img.save(img_path)
+
 
 def process_background(background_path, audio_duration):
     background = VideoFileClip(background_path).loop(duration=audio_duration)
 
     max_start_time = max(0, background.duration - 120)
     start_time = random.uniform(0, max_start_time)
-    background = background.subclip(
-            max_start_time,
-            start_time + audio_duration
-            ).loop(duration=audio_duration)
+    background = background.subclip(max_start_time, start_time + audio_duration).loop(
+        duration=audio_duration
+    )
     return background
 
-def process_captions(t, caption_file, video_duration):
+
+def process_captions(t, caption_file, font):
     subtitles = pysubs2.load(caption_file)
-    captions = [(event.start / 1000, event.end / 1000, event.text) for event in subtitles]
+    captions = [
+        (event.start / 1000, event.end / 1000, event.text) for event in subtitles
+    ]
 
     height, width = 1920, 1080  # Adjust as per your video dimensions
     frame = np.zeros((height, width, 3), dtype=np.uint8)
@@ -176,10 +179,9 @@ def process_captions(t, caption_file, video_duration):
     for start, end, text in captions:
         if start <= t < end:
             # Create an image using PIL
-            img = Image.new('RGB', (width, height), (0, 0, 0))
+            img = Image.new("RGB", (width, height), (0, 0, 0))
             draw = ImageDraw.Draw(img)
 
-            # Load a font
             base_font_size = 125
             font = ImageFont.truetype("m.otf", base_font_size)
 
@@ -189,35 +191,33 @@ def process_captions(t, caption_file, video_duration):
             word_duration = (end - start) / len(words)
             color = (255, 255, 255)  # White color
             for i, word in enumerate(words):
-
                 word_start = start + i * word_duration
-                word_end = word_start + word_duration
 
                 relative_t = max(0, min((t - word_start) / word_duration, 1))
-                pop_scale = 1 + 0.04 * (1 - abs(relative_t - 0.5) * 2)  # adjust the 0.2 as needed
+                pop_scale = 1 + 0.04 * (1 - abs(relative_t - 0.5) * 2)
 
                 font_size = int(base_font_size * pop_scale)
-                font = ImageFont.truetype("m.otf", font_size)
+                font = ImageFont.truetype(font, font_size)
 
                 text_width = draw.textlength(word, font)
                 x = (width - text_width) // 2
                 draw.text((x, y), word, font=font, fill=color)
                 y += base_font_size
 
-
             frame = np.array(img)
             break
 
     return frame
 
-def process_text(post_text):
+
+def process_text(post_text, key):
     data = ""
-    with open('gpt-keys.json', 'r') as keys:
+    with open("gpt-keys.json", "r") as keys:
         data = json.load(keys)
-    
-    openai.api_key = data['API_KEY']    
-    chunks = [post_text[i:i+2000] for i in range(0, len(post_text), 2000)]
-    
+
+    openai.api_key = data["API_KEY"]
+    chunks = [post_text[i : i + 2000] for i in range(0, len(post_text), 2000)]
+
     improved_texts = []
     for chunk in chunks:
         response = openai.ChatCompletion.create(
@@ -225,21 +225,18 @@ def process_text(post_text):
             messages=[
                 {
                     "role": "system",
-                    "content": "Please correct the grammar and make the following text more readable for a text-to-speech voice."
+                    "content": "Please correct the grammar and make the following text more readable for a text-to-speech voice.",
                 },
-                {
-                    "role": "user",
-                    "content": chunk
-                }
-            ]
+                {"role": "user", "content": chunk},
+            ],
         )
 
-        improved_texts.append(response.choices[0].message['content'])
+        improved_texts.append(response.choices[0].message["content"])
 
     post_text = " ".join(improved_texts)
-    post_text.strip().replace("\n", "")
+    post_text = post_text.strip().replace("\n", "")
     return post_text
-       
+
 
 def apply_opacity(get_frame, t):
     frame = get_frame(t)
@@ -248,43 +245,80 @@ def apply_opacity(get_frame, t):
     else:
         return frame
 
+
 def threshold(frame, thr=0.5):
     mask = (frame > thr).astype(np.uint8) * 255  # Convert 1 to 255 for a white mask
     return np.stack([mask] * 3, axis=-1)
 
-def write_title(post_title):
-    with open('filename.txt', 'w') as file:
+
+def write_title(post_title, title_path):
+    with open(title_path, "w") as file:
         file.write(post_title)
 
-def pre_process():
-    reddit = reddit_init()
-    post_title, post_url, post_text = get_post(reddit)
-    post_text = process_text(post_text)
-    write_title(post_title)
 
-    get_voiceover(f"{post_title} {post_text}")
-    get_image("LetsNotMeet Reddit", "LetsNotMeet", post_title)
+def pre_process(config):
+    credentials = {
+        "client_id": config["client_id"],
+        "client_secret": config["client_secret"],
+        "user_agent": config["user_agent"],
+    }
+    reddit = reddit_init(**credentials)
 
-    process_image("post.png")
+    post_title, post_url, post_text = get_post(
+        reddit,
+        sub=config["subreddit"],
+        time_filter=config["time_filter"],
+        seen_path=config["seen_path"],
+    )
+    if config["use_gpt"]:
+        post_text = process_text(post_text, config["gpt_key"])
+    write_title(post_title, title_path=config["title_file"])
 
-def composite():
-    audio = AudioFileClip("voiceover.mp3")
-    img = ImageClip("post.png").set_duration(3)
-    background = process_background("background.mp4", audio.duration) 
+    get_voiceover(
+        f"{post_title} {post_text}",
+        language=config["language"],
+        gender=config["voice_sound"],
+        output_audio=config["voiceover_file"],
+        output_subs=config["captions_file"],
+    )
+    get_image(
+        config["img_username"],
+        config["img_@"],
+        post_title,
+        output_path=config["img_file"],
+    )
+    process_image(config["img_file"])
 
-    captions = VideoClip(lambda t: process_captions(t, "voiceover.vtt", audio.duration), duration=audio.duration)
+
+def composite(config):
+    audio = AudioFileClip(config["voiceover_file"])
+    img = ImageClip(config["img_file"]).set_duration(3)
+    background = process_background(config["background_video"], audio.duration)
+
+    captions = VideoClip(
+        lambda t: process_captions(t, config["captions_file"], config["font"]),
+        duration=audio.duration,
+    )
     captions = captions.fl(apply_opacity)
-    gray_clip = captions.fl_image(lambda frame: cv2.cvtColor(frame, cv2.COLOR_RGBA2GRAY))
+    gray_clip = captions.fl_image(
+        lambda frame: cv2.cvtColor(frame, cv2.COLOR_RGBA2GRAY)
+    )
 
     mask_clip = gray_clip.fl_image(lambda frame: threshold(frame))
     isolated_text_clip = mask_clip.set_mask(mask_clip.to_mask())
     isolated_text_clip = isolated_text_clip.set_position("center")
     captions = isolated_text_clip.set_duration(audio.duration)
 
-    video = CompositeVideoClip([background, img.set_position("center"), captions]).set_audio(audio)
+    video = CompositeVideoClip(
+        [background, img.set_position("center"), captions]
+    ).set_audio(audio)
     video.write_videofile("video.mp4", codec="libx264", fps=24)
+    clean_tmp()
+
 
 if __name__ == "__main__":
-    pre_process()
-    composite()
-    post_to_tiktok("/home/shorts/shorts_generator/video.mp4")
+    config = load_config()
+
+    pre_process(config)
+    composite(config)
+    post_to_tiktok(config["video_path"])
---
+++ src/post.py
@@ -0,0 +1,160 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from fake_useragent import UserAgent
+from selenium_stealth import stealth
+
+import time
+import json
+
+def load_config(config_path='config.json'):
+    with open(config_path, 'r') as f:
+        return json.load(f)
+
+def driver_init(profile_path):
+    """
+    Initializse a Selenium Chrome webdriver with specific options and configurations.
+
+    This function sets up the Chrome webdriver with specified browser options for
+    automation tasks. Additionally, it applies stealth settings to make the browser
+    automation less detectable by anti-bot mechanisms.
+
+    Parameters:
+    - profile_path (str): Path to the Chrome user profile directory. Defaults to "./google-chrome".
+
+    Returns:
+    - WebDriver: A configured instance of the Chrome webdriver.
+
+    Example:
+    driver = driver_init("/path/to/custom-profile")
+    """
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("start_maximized")
+    options.add_argument(f"--user-data-dir={profile_path}")
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    driver = webdriver.Chrome(options=options)
+
+    stealth(
+        driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
+
+    return driver
+
+
+def upload_video(driver, file_path):
+    """
+    Uploads a video using the provided Selenium Chrome webdriver.
+
+    Parameters:
+    - driver (WebDriver): Initialized Chrome webdriver.
+    - file_path (str): Path to the video file.
+
+    Assumes the driver is already on the correct webpage.
+    """
+
+    time.sleep(20)
+
+    driver.switch_to.frame(0)
+    upload_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
+    upload_input.send_keys(file_path)
+    driver.switch_to.default_content()
+
+    time.sleep(250)  # Wait for upload
+
+
+def write_title(driver, title, tags):
+    """
+    Inputs a title and predefined tags into a caption field on a TikTok upload form.
+
+    Parameters:
+    - driver (WebDriver): Initialized Chrome webdriver.
+    - title (str): The text to be written as the video's caption.
+
+    Assumes the driver is already on the correct webpage and at the caption input stage.
+    """
+
+    tags = ["#reddit", "#redditstories"]
+
+    driver.switch_to.frame(0)
+    caption_div = driver.find_element(
+        By.XPATH, '//div[@aria-label="Caption"]//div[@contenteditable="true"]'
+    )
+    caption_div.click()
+
+    for existing in range(20):
+        ActionChains(driver).send_keys(Keys.BACKSPACE).perform()
+        time.sleep(1)
+
+    for letter in title:
+        ActionChains(driver).send_keys(letter).perform()
+        time.sleep(1)
+
+    for tag in tags:
+        ActionChains(driver).send_keys(tag).perform()
+        time.sleep(1)
+        ActionChains(driver).send_keys(Keys.RETURN).perform()
+
+
+def post_video(driver):
+    """
+    Posts the uploaded video on TikTok and navigates to the 'Manage your posts' page.
+
+    Parameters:
+    - driver (WebDriver): Initialized Chrome webdriver.
+
+    Assumes the driver is already on the correct webpage and at the final posting stage.
+    """
+
+    time.sleep(30)
+    post_button = driver.find_element(By.XPATH, '//button[div/div[text()="Post"]]')
+    post_button.click()
+
+    time.sleep(20)
+    manage_button = driver.find_element(By.XPATH, "//*[text()='Manage your posts']")
+    manage_button.click()
+    driver.quit()
+
+
+def post_to_tiktok(
+    file_path, profile_path="./google-chrome", caption_file="filename.txt"
+):
+    """
+    Automate the process of posting a video to TikTok using Selenium.
+
+    Parameters:
+    - file_path (str): Path to the video file to be uploaded.
+    - profile_path (str, optional): Path to the Chrome profile. Defaults to "./google-chrome".
+    - caption_file (str, optional): Path to the file containing the video caption. Defaults to "filename.txt".
+
+    Uses helper functions to initialize the driver, upload the video, set the title, and post the video.
+    """
+
+    config = load_config()
+
+    FILE_PATH = config['file_path']
+    PROFILE_PATH = config['profile_path']
+    TITLE_FILE = config['title_file']
+    TAGS = config['tags']
+    URL = config['url']
+
+    with open(TITLE_FILE, "r") as file:
+        title = file.read()
+
+    driver = driver_init(PROFILE_PATH)
+    driver.get(URL)
+
+    upload_video(driver, FILE_PATH)
+    write_title(driver, title, TAGS)
+    post_video(driver)
+
+    driver.quit()
